064480cfe6d736de6f368d7cbcbc8de49175d5e6
ckdu <51294469+ckdu@users.noreply.github.com>
2022-01-25 21:11:21 +0000

    Create discordpy-self.py

---
+++ discordpy-self.py
@@ -0,0 +1,24 @@
+import discord
+import time
+
+token = "token" # Discord token
+channelToWatch = 0 # Welcome channel ID
+channelToSend = 0 # Channel ID to send the user ids
+ids = ""
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        global ids
+
+        if message.channel.id == channelToWatch:
+            ids += str(message.author.id) + "\n"
+        if ids.count("\n") > 4:
+            sendIds = ids
+            ids = ""
+            await client.get_channel(channelToSend).send(sendIds)
+
+client = MyClient()
+client.run(token)
