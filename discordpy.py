3f32970c6f011cbe273ef6d051261c27c3d50013
ckdu <51294469+ckdu@users.noreply.github.com>
2022-02-03 21:03:46 +0000

    Very simple discord.py bot
    Sends new members ID's in a specified channel.

---
+++ discordpy.py
@@ -0,0 +1,17 @@
+import discord
+
+token = "" # bot token
+channelId = 935399994675048458 # channel id to send new member ids in
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_member_join(self, member):
+        await client.get_channel(channelId).send(member.id)
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = MyClient(intents=intents)
+client.run(token)
